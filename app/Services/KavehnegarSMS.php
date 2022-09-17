45c2ba583a9c6ca694e729a7245524acd4db4c52
m-foroutan <m.foroutan26@yahoo.com>
2022-09-18 01:05:25 +0430

    update Services and add sendSimpleSMS method

--- app/Services/KavehnegarSMS.php
+++ app/Services/KavehnegarSMS.php
@@ -6,12 +6,21 @@ use Kavenegar\KavenegarApi;
 
 class KavehnegarSMS implements SMS
 {
-  public function __construct(protected KavenegarApi $sms) //protected Kavenegar $sms
+  public function __construct(protected KavenegarApi $sms)
   {
   }
 
-  public function sendVerifySMS($receptor, array $tokens, $template, $type = null)
+  public function sendVerifySMS($receptor, $tokens, $template, $type = null)
   {
-    return $this->sms->VerifyLookup($receptor, $tokens[0], $tokens[1], $tokens[2], $template, $type = null);
+    $token1 = $tokens[0];
+    $token2 = $tokens[1] ?? null;
+    $token3 = $tokens[2] ?? null;
+
+    return $this->sms->VerifyLookup($receptor, $token1, $token2, $token3, $template, $type);
+  }
+
+  public function sendSimpleSMS($sender, $receptor, $message)
+  {
+    return $this->sms->Send($sender, $receptor, $message);
   }
 }
--- app/Services/SMS.php
+++ app/Services/SMS.php
@@ -5,4 +5,6 @@ namespace App\Services;
 interface SMS
 {
   public function sendVerifySMS(string $receptor, array $tokens, string $template, string $type = null);
+
+  public function sendSimpleSMS(string $sender, string $receptor, string $message);
 }
